7990da41113459febb264a9c75aa65b4a389b41b
Ivan Shkutov <ivan.shkutov@example.com>
2026-02-14 16:31:47 +0000

    Добавил метод sayHunterReply и тест

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,18 +1,25 @@
 package plaindoll;
 
-public class Welcomer{
-	// Если хочешь больше веселья и информации про ДевОпс - приходи в мои каналы NotOps (telegram, YT, Boosty, Patreon)
-	// https://t.me/notopsofficial
-	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
-	}
-	public String sayFarewell() {
-		return "Farewell, good hunter. May you find your worth in waking world.";
-	}
-	public String sayNeedGold(){
-		return "Not enough gold";
-	}
-	public String saySome(){
-		return "something in the way";
-	}
+public class Welcomer {
+
+    public String sayWelcome() {
+        return "Welcome home, good hunter. What is it your desire?";
+    }
+
+    public String sayFarewell() {
+        return "Farewell, good hunter. May you find your worth in waking world.";
+    }
+
+    public String sayNeedGold(){
+        return "Not enough gold";
+    }
+
+    public String saySome(){
+        return "something in the way";
+    }
+
+    // Новый метод для ветки feature/add_reply
+    public String sayHunterReply() {
+        return "Hunter, I see you approach. Be ready!";
+    }
 }
